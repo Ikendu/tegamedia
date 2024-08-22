b1a0c09d2c2cf3721a94429aa9f0282e208e3dd5
Aniede Chibundu <ndubest56@gmail.com>
2024-08-22 05:06:10 +0100

    show mobile nav

--- src/components/MobileRightNav.jsx
+++ src/components/MobileRightNav.jsx
@@ -21,23 +21,29 @@ export default function MobileRightNav(showMobileNav) {
   };
 
   return (
-    <div className="md:flex md:gap-10 items-center hidden">
-      {navBar.map((item, idx) => (
-        <ul key={idx} className="cursor-pointer">
-          <a
-            href={links[idx]}
-            target="_blank"
-            onClick={() => handleTabClick(item)}
-            className={`hover:text-[#910249] ${
-              selectedTab === item
-                ? "border-l-4 border-[#d61c79] px-2 rounded-lg"
-                : "border-l-4 border-white px-2 rounded-lg"
-            } `}
-          >
-            {item}
-          </a>
-        </ul>
-      ))}
+    <div>
+      {showMobileNav ? (
+        <div className="md:flex md:gap-10 items-center hidden">
+          {navBar.map((item, idx) => (
+            <ul key={idx} className="cursor-pointer">
+              <a
+                href={links[idx]}
+                target="_blank"
+                onClick={() => handleTabClick(item)}
+                className={`hover:text-[#910249] ${
+                  selectedTab === item
+                    ? "border-l-4 border-[#d61c79] px-2 rounded-lg"
+                    : "border-l-4 border-white px-2 rounded-lg"
+                } `}
+              >
+                {item}
+              </a>
+            </ul>
+          ))}
+        </div>
+      ) : (
+        <div>Mobile</div>
+      )}
     </div>
   );
 }
